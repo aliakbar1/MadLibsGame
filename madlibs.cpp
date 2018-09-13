b4d605799ef671cb8edcd8b7844cad5c3b85fd6a
aliakbar1 <aakbar2@mail.csuchico.edu>
2018-09-12 17:54:21 -0700

    did madlib game

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,47 @@
-//Authors: 
+//Authors: Ali Akbar 
 #include <iostream>
 #include <string>
+using namespace std;
 
-int main()
+ int main()       
 {
+string name;
+int number;
+string sillyaction;
+string seriousaction;
+string action;
+string emotion;
+string object;
+std::cout<<"please enter a name";
+std::cin>>name;
+std::cout<<"please enter an object";
+std::cin>>object;
+std::cout<<"please enter an action";
+std::cin>>action;
+std::cout<<"please enter a silly action";
+std::cin>>sillyaction;
+std::cout<<"please enter a number";
+std::cin>>number;
+std::cout<<"please enter a serious action";
+std::cin>>seriousaction;
+std::cout << " Dear Walmart "<<std::endl;
+std::cout << name;
+std::cout <<" would like to return her ";
+std::cout<<object;
+std::cout<<" although her purchase mad her feel ";
+std::cout<<emotion;
+std::cout<<" it ";
+std::cout<<action;
+std::cout<<" so hard it made her mother ";
+std::cout<<sillyaction;
+std::cout<<" please refund her ";
+std::cout<<number;
+std::cout<<" dollars" <<std::endl;
+std::cout<<" thank you"<<std::endl;
+std::cout<<" Dr.";
+std::cout<<seriousaction;
 
-  return 0;
+
+
+return 0;
 }
